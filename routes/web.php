665794cdff336290c3ad8611c6142d22ff7fc572
amfarari <anish233228@gmail.com>
2022-07-29 01:19:23 +0530

    29-07-2022 12:14 AM  Frist Controller TEst

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('go');
 });
-Route::get('/Hii', [Home::class, 'index']);
+Route::get('/Hiis', [Home::class, 'index']);
