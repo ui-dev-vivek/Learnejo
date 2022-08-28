54cb15ba59a76c3fc198565abb6dad86b7a9f0fd
amfarari <anish233228@gmail.com>
2022-08-28 10:48:13 +0530

    hi

--- resources/views/main/whatsapp.blade.php
+++ resources/views/main/whatsapp.blade.php
@@ -1,5 +1,5 @@
 @php
-$title = 'Join Us With Whatsapp';
+$title = 'Learnejo : Join Whatsapp Group';
 $image = asset('assets/image/whatsapp-join.jpg');
 $description = 'Learnejo is an E-learning platform, for every student interested and wishes to prepare for Tech and non-Tech. This platform contains all the important topics and articles related to Tech and Non-Tech. With the help of which you can enhance your skills and knowledge as well as enhance your career.';
 
