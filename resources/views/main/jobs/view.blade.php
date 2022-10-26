83a4973ad687dfd5f29248fab12a36a372a346ab
amfarari <anish233228@gmail.com>
2022-10-26 21:09:22 +0530

    NEw Job Interface

--- resources/views/main/jobs/view.blade.php
+++ resources/views/main/jobs/view.blade.php
@@ -1,7 +1,7 @@
 <x-layout.main.base :title="$job->title" :image="$job->image" :description="$job->title">
     <x-slot name='hero'>
         <!-- ======= Intro Single ======= -->
-        <section class="intro-single">
+        {{-- <section class="intro-single">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12 col-lg-8">
@@ -20,27 +20,25 @@
                 </div>
             </div>
             </div>
-        </section><!-- End Intro Single-->
+        </section><!-- End Intro Single--> --}}
+        <br><br><br>
     </x-slot>
-    <section class="news-single nav-arrow-b  text-justify">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="news-img-box text-center">
-                        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7119514135532896"
-                            data-ad-slot="8356744114" data-ad-format="auto" data-full-width-responsive="true"></ins>
-                        <script>
-                            (adsbygoogle = window.adsbygoogle || []).push({});
-                        </script>
-                    </div>
-                    {{-- <img src="{{ $job->image }}" alt="" class="img-fluid"> --}}
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-8">
+                <div class="shadow p-2 text-center">
+                    <!-- Display-Horizontal -->
+                    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7119514135532896"
+                        data-ad-slot="2511322819" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
                 </div>
-            </div>
-            <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-                <div class="post-content color-text-a">
-                    <p class="post-intro">
-                        <span class="h2">{{ $job->title }}</span>
-                    </p>
+
+                <div class="shadow mt-3 p-3">
+                    <p class="text-center text-primary">
+                    <h1 class="h2">{{ $job->title }}</h1>
                     <div class="post-information">
                         <ul class="list-inline text-center color-a">
                             <li class="list-inline-item mr-2">
@@ -61,27 +59,14 @@
                             </li>
                         </ul>
                     </div>
-                    <p class="text-center">
-                        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7119514135532896"
-                            data-ad-slot="8356744114" data-ad-format="auto" data-full-width-responsive="true"></ins>
-                        <script>
-                            (adsbygoogle = window.adsbygoogle || []).push({});
-                        </script>
-
                     </p>
-
+                    <p class="text-center">
+                        {{-- Horizontal Ads --}}
+                    </p>
                     <p>
                         <?php
                         echo $job->content;
                         ?>
-                    </p>
-                    <p class="text-center">
-                        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7119514135532896"
-                            data-ad-slot="8356744114" data-ad-format="auto" data-full-width-responsive="true"></ins>
-                        <script>
-                            (adsbygoogle = window.adsbygoogle || []).push({});
-                        </script>
-
                     </p>
                     <blockquote class="blockquote">
                         <p class="mb-4">{{ $job->title }} <br>
@@ -92,65 +77,48 @@
                             <cite title="Source Title">Learnejo</cite>
                         </footer>
                     </blockquote>
-                    <div class="text-center">
-                        {{-- <a class="btn btn-md btn-info" href="javascript:void(0);"
-                            onclick="apply('{{ $job->prama_link }}','{{ $job->link }}','{{ csrf_token() }}');"><span
-                                class="apply">Apply
-                                Now </span></a> --}}
-                        <a class="btn btn-md btn-info" href="{{ $job->link }}">To Apply Click Here. </a>
-                    </div>
                     <p class="text-center">
+                        <!-- Display-Horizontal -->
                         <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7119514135532896"
-                            data-ad-slot="8356744114" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                            data-ad-slot="2511322819" data-ad-format="auto" data-full-width-responsive="true"></ins>
                         <script>
                             (adsbygoogle = window.adsbygoogle || []).push({});
                         </script>
-
+                        <a class="btn btn-md btn-info" href="{{ $job->link }}">Click Here To Apply </a>
+                        <!-- Display-Horizontal -->
+                        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7119514135532896"
+                            data-ad-slot="2511322819" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
                     </p>
                 </div>
-                <br>
-                <div class="post-footer">
-                    <div class="post-share">
-                        <span>Share: </span>
-                        <ul class="list-inline socials">
-                            <li class="list-inline-item">
-                                <a href="#">
-                                    <i class="bi bi-facebook" aria-hidden="true"></i>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#">
-                                    <i class="bi bi-twitter" aria-hidden="true"></i>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#">
-                                    <i class="bi bi-instagram" aria-hidden="true"></i>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#">
-                                    <i class="bi bi-linkedin" aria-hidden="true"></i>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+            </div>
+            <div class="col-4 ps-2">
+
+                <div class="shadow p-2 text-center">
+                    {{-- Squir Ads --}}
+                    <!-- Display-Square -->
+                    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7119514135532896"
+                        data-ad-slot="7763649496" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div><br>
+                <div class="shadow p-2 text-center">
+                    {{-- Virtical Ads --}}
+                    <!-- Display-Vertical -->
+                    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7119514135532896"
+                        data-ad-slot="1712412310" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
                 </div>
+
             </div>
         </div>
-        <br>
 
-        </div>
-        </div>
-        <p class="text-center">
-            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7119514135532896"
-                data-ad-slot="8356744114" data-ad-format="auto" data-full-width-responsive="true"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
-
-        </p>
-    </section><!-- End Blog Single-->
+    </div>
     @section('script')
         <script src="{{ asset('assets/ajax/apply-now.js') }}"></script>
     @stop
