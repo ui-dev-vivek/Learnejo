04cfaf0d6c39bc1ada0b360b1125904c644c5d2d
amfarari <anish233228@gmail.com>
2022-11-02 18:04:17 +0530

    hi

--- resources/views/main/jobs/view.blade.php
+++ resources/views/main/jobs/view.blade.php
@@ -59,6 +59,19 @@
                             </li>
                         </ul>
                     </div>
+                    <div class="p-2">
+                        <div
+                            style="position: relative; width: 100%; height: 0; padding-top: 33.3333%;
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+ border-radius: 8px; will-change: transform;">
+                            <iframe loading="lazy"
+                                style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+                                src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFQzNFKre0&#x2F;view?embed"
+                                allowfullscreen="allowfullscreen" allow="fullscreen">
+                            </iframe>
+                        </div>
+
+                    </div>
                     </p>
                     <p class="text-center">
                         {{-- Horizontal Ads --}}
