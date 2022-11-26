0498fbf99aaf7291e5ce00876b6417fafa218be1
amfarari <anish233228@gmail.com>
2022-11-26 15:04:18 +0530

    Adding Python Script

--- resources/views/main/sitemap.blade.php
+++ resources/views/main/sitemap.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($courses as $course)
         <url>
-            <loc>{{ url($course->prama_link) }}</loc>
+            <loc>{{ url('Courses') }}/{{ $course->prama_link }}</loc>
             <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z', strtotime($course->updated_at)) }}</lastmod>
             <changefreq>Hourly</changefreq>
             <priority>0.8</priority>
@@ -10,10 +10,18 @@
     @endforeach
     @foreach ($jobs as $job)
         <url>
-            <loc>{{ url($job->prama_link) }}</loc>
+            <loc>{{ url('Job-Internships') }}/{{ $job->prama_link }}</loc>
             <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z', strtotime($job->updated_at)) }}</lastmod>
             <changefreq>Hourly</changefreq>
             <priority>0.8</priority>
         </url>
     @endforeach
+    {{-- @foreach ($prep as $prep)
+        <url>
+            <loc>{{ url($prep->prama_link) }}</loc>
+            <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z', strtotime($job->updated_at)) }}</lastmod>
+            <changefreq>Hourly</changefreq>
+            <priority>0.8</priority>
+        </url>
+    @endforeach --}}
 </urlset>
